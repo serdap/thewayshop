c6359e3cf2743b5e53c36d4caea14e27c89d0651
hhhoanganh <101188848+hhhoanganh@users.noreply.github.com>
2023-12-21 23:10:04 +0700

    Update searchSuggestion.js

--- js/searchSuggestion.js
+++ js/searchSuggestion.js
@@ -7,13 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
         suggestionsList.innerHTML = '';
 
         console.log(value)
-        url = 'http://127.0.0.1:8000/api/v1/products/search';
+        url = `http://127.0.0.1:8000/api/v1/search?value=${value}`;
         const options = {
-            method: "POST",
+            method: "GET",
             headers: {
                 'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({value})
+            }
         }
 
         // Gọi API với từ khóa tìm kiếm
@@ -21,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(response => response.json())
             .then(data => {
                 const suggestions = data.data;
+                console.log(data)
 
                 suggestions.forEach(suggestion => {
                     const li = document.createElement("li");
@@ -28,6 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     li.addEventListener("click", function () {
                         searchInput.value = suggestion.name;
                         suggestionsList.style.display = "none";
+                        displayProducts(suggestion.id);
                     });
                     suggestionsList.appendChild(li);
                 });
@@ -42,4 +43,62 @@ document.addEventListener("DOMContentLoaded", function () {
             suggestionsList.style.display = "none";
         }
     });
+    function displayProducts(suggestionId) {
+        // Assuming you have a function to fetch and display products based on the suggestion ID
+        const productsUrl = `http://127.0.0.1:8000/api/v1/products/${suggestionId}`;
+        fetch(productsUrl)
+            .then(response => response.json())
+            .then(productsData => {
+                // Display products in the product container
+                productContainerDetail.innerHTML = ''; // Clear existing content
+                productContainer.innerHTML = '';
+                productsData.data.forEach(product => {
+                    const productCard = document.createElement("div");
+                    productCard.className = 'list-view-box';
+            // ${item.thumbnail}
+                    productCard.innerHTML = `
+                    <div class="row">
+                        <div class="col-sm-6 col-md-6 col-lg-4 col-xl-4">
+                            <div class="products-single fix">
+                                <div class="box-img-hover">
+                                    <div class="type-lb">
+                                        <p class="sale">Sale</p>
+                                    </div>
+                                    <img src="images/data_${item.thumbnail}.jpg" class="img-fluid" alt="Image">
+                                    <div class="mask-icon">
+                                        <ul>
+                                            <li><a href="#" data-toggle="tooltip"
+                                                data-placement="right" title="View"><i
+                                                    class="fas fa-eye"></i></a></li>
+                                            <li><a href="#" data-toggle="tooltip"
+                                                data-placement="right" title="Compare"><i
+                                                    class="fas fa-sync-alt"></i></a></li>
+                                            <li><a href="#" data-toggle="tooltip"
+                                                data-placement="right"
+                                                title="Add to Wishlist"><i
+                                                    class="far fa-heart"></i></a></li>
+                                        </ul>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-6 col-lg-8 col-xl-8">
+                            <div class="why-text full-width">
+                                <h4>${product.name}</h4>
+                                <h5> $${product.price}</h5>
+                                <p>${product.description}</p>
+                                <a class="btn hvr-hover" href="#">Add to Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                `;
+
+                    productContainerDetail.appendChild(productCard);
+                });
+                // Show the product container
+                document.getElementById("list-view-searcch").classList.add("show");
+            })
+            .catch(error => console.error("Error fetching products:", error));
+    }
 });
